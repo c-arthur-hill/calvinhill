7e9dcb4f94c34687b858f6b7bd73f3356b631c6b
c-arthur-hill <c.arthur.hill@gmail.com>
2023-05-15 20:00:30 -0400

    updating about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-5 offset-lg-4 mb-5">
                 <h3>Professional</h3>
                 <p>My first tech job was troubleshooting corporate networks for Presidio Networked Solutions.</p>
-                <p>I worked at Seminole State College and Florida State University as a web application developer. I continued taking classes to improve my skills.</p>
+                <p>I worked at Seminole State College and Florida State University as a web application developer.</p>
                 <p>I started contracting with a private agency in Clearwater, FL. I helped build online fulfillment software for the apparel industry. I also led development of a portal for calculating sales commissions.</p>
                 <h3>Personal</h3>
                 <p>I drive a Corolla. I workout with kettlebells and eat oatmeal.</p>
