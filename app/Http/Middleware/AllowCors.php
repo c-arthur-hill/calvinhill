e956b23ba55aaf2a3b9bd5418a8051e7f5d93059
c-arthur-hill <c.arthur.hill@gmail.com>
2023-05-29 17:10:07 -0400

    cors mod

---
+++ app/Http/Middleware/AllowCors.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AllowCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $allowedOrigins = ['localhost', 'www.calvinhill.com', 'calvinhill.com'];
+        $origin = $_SERVER['HTTP_ORIGIN'];
+        if (in_array($origin, $allowedOrigins)) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $origin)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type');
+        }
+
+        return $next($request);
+    }
+}
