b2ab793184c8581c5ee8777663e7722e4efcf172
c-arthur-hill <c.arthur.hill@gmail.com>
2023-05-29 18:52:30 -0400

    cors mod

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.calvinhill.com', 'calvinhill.com'],
+    'allowed_origins' => ['*.calvinhill.com', 'https://calvinhill.com'],
 
     'allowed_origins_patterns' => [],
 
