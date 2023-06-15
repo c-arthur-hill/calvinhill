a1a6c82d3a94ebccf0dad3807edf4644700d8cbd
c-arthur-hill <c.arthur.hill@gmail.com>
2023-06-15 15:31:50 -0400

    updating front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,13 +9,13 @@
                     <div  class="col-lg-8 p-5">
                         <div class="row">
                             <div class="col-lg-12">
-                                <h1 class="display-5">Custom web application development</h1>
-                                <p class="text-secondary mt-3">Can I <a href="https://calendly.com/calvinhillsoftware/30min" target="_blank">schedule a 30 minute meeting</a> to understand your current software or business process?</p>
+                                <h1 class="display-5">A custom software application development company</h1>
+                                <p class="text-secondary mt-3"><i>American. Experienced. Successful.</i></p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4">
-                        <form class="p-3 bg-light border" method="POST" action="{{ route('contact.update') }}">
+                        <form class="p-3 bg-light border h-100" method="POST" action="{{ route('contact.update') }}">
                             <h3>Contact</h3>
                             @if ($errors->any())
                                 <div class="alert alert-danger border-danger rounded-0">
@@ -57,33 +57,12 @@
                 </div>
                 <div class="col-lg-5 offset-lg-1">
                     <h5 class="mt-5 mt-lg-0">Services</h5>
-                    <p><mark>I develop custom web applications.</mark> I start new or upgrade existing.</p>
-                    <p>I don't have 700 customers.<p>
-                    <p>I don't do desktop. I don't do native mobile. My web apps work fine across phone and desktop browsers.</p>
-                    <p>I don't run marketing campaigns.</p>
-                    <!--
-                    <form class="p-3 bg-light border mt-5 mb-5" method="POST" action="{{ route('contact.update') }}">
-                        @csrf
-                        <h3>Ready now?</h3>
-                        <div class="form-floating">
-                            <input id="email" type="email" name="email" class="form-control rounded-0">
-                            <label for="email" class="">Email*</label>
-                        </div>
-                        <div class="form-floating mt-3">
-                            <input id="name" type="text" name="name" class="form-control rounded-0">
-                            <label for="name" class="">Name</label>
-                        </div>
-                        <div class="form-floating mt-3">
-                            <textarea id="description" name="description" class="form-control rounded-0"></textarea>
-                            <label for="description" class="">Description</label>
-                        </div>
-                        <div class="mt-3">
-                            <button type="submit" class="btn btn-danger rounded-0">Request services</button>
-                        </div>
-                    </form>
-                    -->
+                    <p><mark>I develop custom software applications.</mark></p>
+                    <p>I start new or upgrade existing.</p>
+                    <p>I gather requirements, design the user-interface and create a step-by-step plan. I develop the code. I deploy the application to servers. I fix any problems that arise.</p>
+                    <p></p>
                     <div class="mt-5 mb-5">
-                        <p class="text-secondary">Tallahassee, FL<br>calvin@calvinhill.com</p>
+                        <p class="text-secondary">Tallahassee, FL, USA<br>calvin@calvinhill.com</p>
                     </div>
                 </div>
             </div>
