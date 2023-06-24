43b7a33a599f119edcb5fa68c98e4767354dd250
c-arthur-hill <c.arthur.hill@gmail.com>
2023-06-24 14:13:22 -0400

    front page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,14 +9,14 @@
                     <div  class="col-lg-8 p-5">
                         <div class="row">
                             <div class="col-lg-12">
-                                <h1 class="display-5">A custom software application development company</h1>
-                                <p class="text-secondary mt-3"><i>American. Experienced. Professional.</i></p>
+                                <h1 class="display-4">Software application development</h1>
+                                <p class="text-secondary mt-3"><i>Organized. Professional. Experienced.</i></p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <form class="p-3 bg-light border h-100" method="POST" action="{{ route('contact.update') }}">
-                            <h3>Contact</h3>
+                            <h3>Request services</h3>
                             @if ($errors->any())
                                 <div class="alert alert-danger border-danger rounded-0">
                                     <ul class="list-unstyled">
@@ -39,7 +39,7 @@
                                 <label for="description" class="">Project description*</label>
                             </div>
                             <div class="mt-3">
-                                <button type="submit" class="btn btn-danger rounded-0">Get started</button>
+                                <button type="submit" class="btn btn-danger rounded-0">Request services</button>
                             </div>
                         </form>
                     </div>
@@ -58,8 +58,7 @@
                 <div class="col-lg-5 offset-lg-1">
                     <h5 class="mt-5 mt-lg-0">Services</h5>
                     <p><mark>I develop custom software applications.</mark></p>
-                    <p>I understand the requirements. I design the interface and create a step-by-step plan. I write the code. I deploy the servers. I fix what breaks.</p>
-                    <p></p>
+                    <p>I gather the requirements. I design the interface and create a step-by-step plan. I write the code. I deploy the servers. I fix what breaks.</p>
                     <div class="mt-5 mb-5">
                         <p class="text-secondary">Tallahassee, FL, USA<br>calvin@calvinhill.com</p>
                     </div>
